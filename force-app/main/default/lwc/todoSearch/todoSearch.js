9fff21badf67046a1a5678eab0f269d39814eba9
roman.zlsk <s0m3c00l3mailadr3ss@gmail.com>
2021-09-16 22:58:50 +0300

    some lwc functionality improvements

---
+++ force-app/main/default/lwc/todoSearch/todoSearch.js
@@ -0,0 +1,118 @@
+import {LightningElement, wire, track} from 'lwc';
+import findTodo from '@salesforce/apex/ToDoHandler.findTodosWithSubTodos';
+import {getPicklistValues} from 'lightning/uiObjectInfoApi';
+import {getObjectInfo} from 'lightning/uiObjectInfoApi';
+import TODO_OBJECT from '@salesforce/schema/ToDo__c';
+import Priority_FIELD from '@salesforce/schema/ToDo__c.Priority__c';
+import isDone_FIELD from '@salesforce/schema/ToDo__c.Is_Done__c';
+
+export default class TodoSearch extends LightningElement {
+    nameKey = '';
+    @track isDone = '';
+    @track isDoneOptions = [];
+    @track priorityKey = '';
+    @track optionsForPriority = [];
+
+    @wire(findTodo, {priorityKey: '$priorityKey', nameKey: '$nameKey'})
+    todos;
+
+    handleChange(event) {
+        window.clearTimeout(this.delayTimeout);
+        const nameKey = event.target.value;
+        this.delayTimeout = setTimeout(() => {
+            this.nameKey = nameKey;
+        }, 300);
+    }
+
+    show = false;
+
+    handleChangeShow(event) {
+        this.show = event.target.checked;
+    }
+
+
+    @wire(getObjectInfo, {objectApiName: TODO_OBJECT})
+    objectInfo;
+
+    @wire(getPicklistValues, {recordTypeId: '$objectInfo.data.defaultRecordTypeId', fieldApiName: Priority_FIELD})
+    typePicklistValuesDone({error, data}) {
+        if (data) {
+            let optionsValues = [];
+            for (let i = 0; i < data.values.length; i++) {
+                optionsValues.push({
+                    label: data.values[i].label,
+                    value: data.values[i].value
+                })
+            }
+            optionsValues.push({
+                label: 'All',
+                value: ''
+            })
+            this.optionsForPriority = optionsValues;
+            window.console.log('optionsValues ===> ' + JSON.stringify(optionsValues));
+        } else if (error) {
+            window.console.log('error ===> ' + JSON.stringify(error));
+        }
+    }
+
+    handleChangePriority(event) {
+        window.clearTimeout(this.delayTimeout);
+        const priorityKey = event.target.value;
+        this.delayTimeout = setTimeout(() => {
+            this.priorityKey = priorityKey;
+        }, 300);
+        //this.priorityKey = event.detail.value;
+    }
+
+    //rework this for isDone checkbox
+    @wire(getPicklistValues, {recordTypeId: '$objectInfo.data.defaultRecordTypeId', fieldApiName: isDone_FIELD})
+    typePicklistValues({error, data}) {
+        if (data) {
+            let optionsValues = [];
+            for (let i = 0; i < data.values.length; i++) {
+                optionsValues.push({
+                    label: data.values[i].label,
+                    value: data.values[i].value
+                })
+            }
+            this.isDoneOptions = optionsValues;
+            window.console.log('optionsValues ===> ' + JSON.stringify(optionsValues));
+        } else if (error) {
+            window.console.log('error ===> ' + JSON.stringify(error));
+        }
+    }
+
+    handleChangeDone(event) {
+        window.clearTimeout(this.delayTimeout);
+        const isDone = event.target.value;
+        this.delayTimeout = setTimeout(() => {
+            this.isDone = isDone;
+        }, 300);
+        //this.priorityKey = event.detail.value;
+    }
+
+    /*handleChangePriority(event) {
+        window.clearTimeout(this.delayTimeout);
+        const priorityKey = event.target.value;
+        this.delayTimeout = setTimeout(() => {
+            this.priorityKey = priorityKey;
+        }, 300);
+    }
+
+    get optionsForPriority() {
+        return [
+            {label: 'High', priorityKey: 'High'},
+            {label: 'Medium', priorityKey: 'Medium'},
+            {label: 'Low', priorityKey: 'Low'},
+            {label: 'All', priorityKey: ''},
+        ];
+    }
+
+    get optionsForDone() {
+        return [
+            {label: 'Done', isDone: 'TRUE'},
+            {label: 'Not done', isDone: 'FALSE'},
+            {label: 'All', isDone: ''},
+        ];
+    }*/
+}
