adac1b667eb6b8e8e5563edf484754d4d5ee2732
nol1pa <volodka.remark.1488@gmail.com>
2021-09-21 13:58:06 +0300

    backup

--- src/lwc/todoDetails/todoDetails.js
+++ src/lwc/todoDetails/todoDetails.js
@@ -1,28 +1,63 @@
 import { LightningElement, api, wire, track } from 'lwc';
 import getSubtodosOfTodo from '@salesforce/apex/SubToDoHandler.getSubToDosOfTodo';
+import getTodo from '@salesforce/apex/ToDoHandler.getTodoById';
+import { subscribe, MessageContext } from 'lightning/messageService';
+import TODO_SELECTED_CHANNEL from '@salesforce/messageChannel/Todo_Selected__c';
+
 
 export default class TodoDetails extends LightningElement {
-    @api todo;
-    recordSubtodosModalOpened = false;
+    todo = '';
+    selectedTodoId = '';//получается из message channel
+    subscribtion = null;
     subtodos = '';
-    todoId = '';
 
-    @wire(getSubtodosOfTodo, {todoId : '$todoId'}) 
-    getSubTodos(result){
-        this.res = result;
+    
+    @wire(MessageContext)
+    messageContext;
+    
+
+
+    @wire(getTodo, { todoId: '$selectedTodoId'})
+    getTodo(result){
+        console.log(result.data);
         if(result.data){
-            this.subtodos = result.data;
+            this.todo = result.data;
         }
     }
-    
-    /*connectedCallback(event){
-        this.todoId = event.data.todo.Id;
-    }*/
 
-    closeSubtodosModal(){
-        this.recordSubtodosModalOpened = false;
+    @wire(getSubtodosOfTodo, {todoId : '$selectedTodoId'}) 
+    getSubTodos(result){
+        if(result.data){
+           this.subtodos = result.data;
+        }
     }
-    openSubtodosModal(){
-        this.recordSubtodosModalOpened = true;
+
+    subscribeToMessageChannel(){
+        this.subscribtion = subscribe(
+            this.messageContext,
+            TODO_SELECTED_CHANNEL,
+            (message)=>this.handleMessage(message)
+        );
     }
+
+    handleMessage(message){
+        this.selectedTodoId = message.selectedTodoId;
+    }
+
+    connectedCallback(){
+        this.subscribeToMessageChannel();
+    }
+
+    get isSubTodosEmpty(){
+        return this.subtodos == '' ? true : false;
+    }
+
+    get isTodoEmpty(){
+        return this.todo == '' ? true : false;
+    }
+
+   
+
+    
+
 }
