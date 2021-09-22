32fe6081982516b49fca8f1cc93c7237565224a9
nol1pa <volodka.remark.1488@gmail.com>
2021-09-22 03:30:03 +0300

    reformat todosearch

--- src/lwc/todoSearch/todoSearch.js
+++ src/lwc/todoSearch/todoSearch.js
@@ -31,7 +31,6 @@ export default class TodoSearch extends LightningElement {
     }
 
     handleDateKeyChange(event){
-        //this.startDateKey = event.target.value.toISOString() + 'T00:00:00';
         const endDateKey = event.target.value;
         this.startDateKey = endDateKey + 'T00:00:00Z';
         this.endDateKey = endDateKey + 'T23:59:59Z';
@@ -65,35 +64,6 @@ export default class TodoSearch extends LightningElement {
         }));
     }
 
-    // @track nameKey = '';
-    // @track priorityKey = '';
-    // @track optionsForPriority = [];
-    // @track startDateKey = '2000-01-01T00:00:00Z';
-    // @track endDateKey = '';
-
-    // @api
-    // get key(){
-    //     return this.key;
-    // }
-    // set key(value){
-    //     this.key.priorityFKey = value.priorityFKey;
-    //     this.key.nameFKey = value.nameFKey;
-    //     this.key.startDateFKey = value.startDateFKey;
-    //     this.key.endDateFKey = value.endDateFKey;
-    // }
-
-    // @wire(findTodo, {
-    //     priorityKey: '$priorityKey',
-    //     nameKey: '$nameKey',
-    //     startDateKey: '$startDateKey',
-    //     endDateKey: '$endDateKey',
-    // })
-    // todos;
-
-    // refresh(){
-    //     refreshApex(this.refresh);
-    // }
-    //
     connectedCallback(){
         const today = new Date();
         this.startDateKey = '2000-01-01T00:00:00Z'
@@ -101,98 +71,5 @@ export default class TodoSearch extends LightningElement {
         this.priorityKey = '';
         console.log(today.toISOString());
     }
-    // renderedCallback() {
-    //     const today = new Date();
-    //     this.startDateKey = '2000-01-01T00:00:00Z'
-    //     this.endDateKey=today.toISOString();
-    //     console.log(today.toISOString());
-    // }
-
-    //
-    // handleChange(event) {
-    //     const lwcEvent3 = new CustomEvent('eventname', {
-    //         detail:{nameKey:this.nameKey}
-    //     });
-    //     this.dispatchEvent(lwcEvent3);
-    //     window.clearTimeout(this.delayTimeout);
-    //     const nameKey = event.target.value.nameKey;
-    //     this.delayTimeout = setTimeout(() => {
-    //         this.nameKey = nameKey;;
-    //     }, 300);
-    //     this.refresh();
-    // }
-
-    show = false;
-
-    handleChangeShow(event) {
-        this.show = event.target.checked;
-    }
-
-
-    // handleClick(){
-    //     const lwcEvent4 = new CustomEvent('eventclick', {
-    //         detail:{endDateKey:this.endDateKey}
-    //     });
-    //     this.dispatchEvent(lwcEvent4);
-    //     const today = new Date();
-    //     this.endDateKey=today.toISOString();
-    //     console.log(today.toISOString());
-    //     this.refresh();
-    // }
-    //
-    //
-    // handleChangeDate(event) {
-    //     const lwcEvent2 = new CustomEvent('eventdate', {
-    //         detail:{endDateKey:this.endDateKey}
-    //     });
-    //     this.dispatchEvent(lwcEvent2);
-    //     window.clearTimeout(this.delayTimeout);
-    //     const endDateKey = event.target.value.toISOString();
-    //     this.delayTimeout = setTimeout(() => {
-    //         this.startDateKey = endDateKey + 'T00:00:00Z';
-    //     }, 300);
-    //     this.delayTimeout = setTimeout(() => {
-    //         this.endDateKey = endDateKey + 'T23:59:59Z';
-    //     }, 300);
-    //     this.refresh();
-    // }
-
-
-    // @wire(getObjectInfo, {objectApiName: TODO_OBJECT})
-    // objectInfo;
-    //
-    // @wire(getPicklistValues, {fieldApiName: Priority_FIELD, recordTypeId: '$objectInfo.data.defaultRecordTypeId'})
-    // typePicklistValuesPriority({error, data}) {
-    //     if (data) {
-    //         let optionsValues = [];
-    //         for (let i = 0; i < data.values.length; i++) {
-    //             optionsValues.push({
-    //                 label: data.values[i].label,
-    //                 value: data.values[i].value
-    //             })
-    //         }
-    //         optionsValues.push({
-    //             label: 'All',
-    //             value: ''
-    //         })
-    //         this.optionsForPriority = optionsValues;
-    //         window.console.log('optionsValues ===> ' + JSON.stringify(optionsValues));
-    //     } else if (error) {
-    //         window.console.log('error ===> ' + JSON.stringify(error));
-    //     }
-    // }
-    //
-    // handleChangePriority(event) {
-    //     const lwcEvent1 = new CustomEvent('eventpriority', {
-    //         detail:{priorityKey:this.priorityKey}
-    //     });
-    //     this.dispatchEvent(lwcEvent1);
-    //     window.clearTimeout(this.delayTimeout);
-    //     const priorityKey = event.target.value;
-    //     this.delayTimeout = setTimeout(() => {
-    //         this.priorityKey = priorityKey;
-    //     }, 300);
-    //     this.refresh();
-    // }
 
 }
